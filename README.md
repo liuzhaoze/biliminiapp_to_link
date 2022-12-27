b11e5a12224ca91fea9a65dfe7164771b14f4324
liuzhaoze <1045954863@qq.com>
2022-12-27 11:48:19 +0800

    repo init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bilibili小程序转链接
+
+## 使用方法
+
+1. 将本仓库clone到 `hoshino/modules/` 目录下
+2. 在 `hoshino/config/__bot__.py` 的对应位置添加该插件
+
+## 仓库链接
+
+<https://github.com/liuzhaoze/biliminiapp_to_link>
---
+++ biliminiapp_to_link.py
@@ -0,0 +1,29 @@
+from hoshino import Service
+from aiocqhttp.event import Event
+import ujson
+import requests
+
+sv = Service('biliminiapp', enable_on_default=True, help_='Bilibili小程序转链接')
+
+
+@sv.on_message('group')
+async def biliminiapp_to_link(bot, event: Event):
+    msg = str(event.message)
+    data = msg[msg.find('data=') + len('data='): -1].replace('&#44;', ',')  # 获取小程序 JSON 信息
+    try:
+        data_j = ujson.loads(data)
+    except ujson.JSONDecodeError:
+        return # 非小程序信息，函数返回
+
+    # 提取视频标题及URL
+    if data_j['desc'] == '哔哩哔哩':
+        title = data_j['meta']['detail_1']['desc']
+        url_raw = data_j['meta']['detail_1']['qqdocurl']
+
+        # 获取实际BV号的URL
+        response = requests.get(url_raw)
+        headers = response.history[0].headers
+        url = headers['Location']
+        url = url[:url.find('?')]
+
+        await bot.send(event, f'{title}\n{url}')
